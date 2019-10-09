912e3421b09e6052cfb4bb2223984ac1d268319e
nika <pinskaya.nika@mail.ru>
2019-10-09 13:47:42 +0300

    Scripted auto-commit on change (2019-10-09 13:47:42) by gitwatch.sh

--- src/main/java/com/examples/slide/WritableTest.java
+++ src/main/java/com/examples/slide/WritableTest.java
@@ -2,11 +2,28 @@ package com.examples.slide;
 
 import java.io.DataInput;
 import java.io.DataOutput;
+import java.io.IOException;
 
-public class WritableTest {
+//public class WritableTest {
     public interface Writable {
         void readFields(DataInput, in);
         void write (DataOutput, out);
-
     }
-}
+    public class add implements Writable {
+        public int a;
+        public int b;
+        public add() {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void write(DataOutput out) throws IOException {
+            out.writeInt(a);
+            out.writeInt(b);
+        }
+
+        public void readFields(DataInput in) throws IOException {
+            
+        }
+    }
+//}
