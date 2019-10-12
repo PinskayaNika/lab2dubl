ac351b76e53b4c15defe60f641f39bcaf728dbfb
nika <pinskaya.nika@mail.ru>
2019-10-12 11:11:12 +0300

    Scripted auto-commit on change (2019-10-12 11:11:12) by gitwatch.sh

--- src/main/java/com/examples/reduceside/JoinReducer.java
+++ src/main/java/com/examples/reduceside/JoinReducer.java
@@ -1,8 +1,14 @@
 package com.examples.reduceside;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class JoinReducer extends Reducer {
-    
+import org.apache.hadoop.io.Text;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class JoinReducer extends Reducer<TextPair, Text,  {
+
 
 }
