a26b85c30121af62b1fac40873e093b315e58d39
nika <pinskaya.nika@mail.ru>
2019-10-12 11:16:47 +0300

    Scripted auto-commit on change (2019-10-12 11:16:47) by gitwatch.sh

--- src/main/java/com/examples/reduceside/JoinReducer.java
+++ src/main/java/com/examples/reduceside/JoinReducer.java
@@ -1,14 +1,17 @@
 package com.examples.reduceside;
 
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import org.apache.hadoop.io.Text;
 import java.io.IOException;
 import java.util.Iterator;
 
-public class JoinReducer extends Reducer<TextPair, Text,  {
+public class JoinReducer extends Reducer<TextPair, Text, Text, Text>  {
+    @Override
+    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        Text
+    }
 
 
 }
