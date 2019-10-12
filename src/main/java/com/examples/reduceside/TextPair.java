4c2019d521f93308dcedf4ecf30a60fcd361a7ec
nika <pinskaya.nika@mail.ru>
2019-10-12 11:56:01 +0300

    Scripted auto-commit on change (2019-10-12 11:56:01) by gitwatch.sh

--- src/main/java/com/examples/reduceside/TextPair.java
+++ src/main/java/com/examples/reduceside/TextPair.java
@@ -5,6 +5,13 @@ import org.apache.hadoop.io.WritableComparable;
 
 public class TextPair implements WritableComparable<TextPair> {
     public Text first;
+    public Text second;
+
+    public TextPair() {
+        this.first = new Text();
+        this.second = new Text();
+    }
+    
 
 
 }
