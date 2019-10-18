09d11a4d81356c9fa779ed7bf0a55a765459730e
nika <pinskaya.nika@mail.ru>
2019-10-18 17:52:14 +0300

    Scripted auto-commit on change (2019-10-18 17:52:14) by gitwatch.sh

--- src/main/java/com/examples/reduceside/GroupingComparatorClass.java
+++ src/main/java/com/examples/reduceside/GroupingComparatorClass.java
@@ -1,9 +1,12 @@
 package com.examples.reduceside;
 
 import org.apache.hadoop.io.RawComparator;
+import org.apache.hadoop.io.WritableComparable;
 
 //Учитывает только код аэропорта
 public class GroupingComparatorClass implements RawComparator {
-
+    public int compare(WritableComparable a, WritableComparable b) {
+        return a.compareTo(b);
+    }
 }
 
