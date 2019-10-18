700e260f8518bfa0322106cd030a1f2b3efd4150
nika <pinskaya.nika@mail.ru>
2019-10-18 17:41:10 +0300

    Scripted auto-commit on change (2019-10-18 17:41:10) by gitwatch.sh

--- src/main/java/com/examples/reduceside/SystemsJoinMapper.java
+++ src/main/java/com/examples/reduceside/SystemsJoinMapper.java
@@ -1,4 +1,16 @@
 package com.examples.reduceside;
 
-public class SystemsJoinMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class SystemsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        SystemInfo system = new SystemInfo(value);
+        context.write(new TextPair(system.getSystemCode().toString(), "0"), new Text(system.toString()));
+    }
+
 }
