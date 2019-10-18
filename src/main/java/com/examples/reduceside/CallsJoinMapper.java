1e2bff59aebb16ce84af4b58dec1f1a532fe3773
nika <pinskaya.nika@mail.ru>
2019-10-18 17:29:21 +0300

    Scripted auto-commit on change (2019-10-18 17:29:21) by gitwatch.sh

--- src/main/java/com/examples/reduceside/CallsJoinMapper.java
+++ src/main/java/com/examples/reduceside/CallsJoinMapper.java
@@ -5,8 +5,13 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class CallsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
-    protected void map(LongWritable key, Text value, Context context,)
-
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        ServiceCall call = new ServiceCall(value);
+        context.write(new TextPair(call.getSystemA().toString(), "1"),
+                new Text(call.toString()));
+    }
 }
