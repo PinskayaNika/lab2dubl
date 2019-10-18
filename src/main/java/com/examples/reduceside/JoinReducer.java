acef40a89c680909f2435c2007ded9201d6e367e
nika <pinskaya.nika@mail.ru>
2019-10-19 00:37:40 +0300

    Scripted auto-commit on change (2019-10-19 00:37:40) by gitwatch.sh

--- src/main/java/com/examples/reduceside/JoinReducer.java
+++ src/main/java/com/examples/reduceside/JoinReducer.java
@@ -11,6 +11,9 @@ public class JoinReducer extends Reducer<TextPair, Text, Text, Text>  {
     @Override
     protected void reduce(TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         int counter = 0;
+        Double min = Double.MAX_VALUE;
+        Double max = Double.MIN_VALUE;
+        Double currentDelay, sum = 0.0;
 
         Iterator<Text> iter = values.iterator();
         Text systemInfo = new Text(iter.next());
