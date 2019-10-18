8b0352547438f3ccbf775ff869a94575f0fc1294
nika <pinskaya.nika@mail.ru>
2019-10-18 16:57:41 +0300

    Scripted auto-commit on change (2019-10-18 16:57:41) by gitwatch.sh

--- src/main/java/com/examples/reduceside/Slide.java
+++ src/main/java/com/examples/reduceside/Slide.java
@@ -23,6 +23,8 @@ public class Slide {
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
 
 
+        //fileInput
+
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setPartitionerClass(TextPair.FirstPartitioner.class);
         job.setGroupingComparatorClass(TextPair.FirstComparator.class);
