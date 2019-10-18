e1f0c3c56c80f7f57a6a563eaa2782e52a0d29e7
nika <pinskaya.nika@mail.ru>
2019-10-18 22:58:03 +0300

    Scripted auto-commit on change (2019-10-18 22:58:03) by gitwatch.sh

--- src/main/java/com/examples/reduceside/SystemsJoinMapper.java
+++ src/main/java/com/examples/reduceside/SystemsJoinMapper.java
@@ -1,15 +1,21 @@
 package com.examples.reduceside;
 
+import javafx.util.Pair;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class SystemsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text {
+public class SystemsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        SystemInfo system = new SystemInfo(value);
-        context.write(new TextPair(system.getSystemCode().toString(), "0"), new Text(system.toString()));
+        Writable airportWritable = new Writable(value.toString());
+        Pair<String, String> airportPair = Writable.getAiroportPair();
+        try {
+            context.write(new TextPair(airportPair.getKey(), "0"), new Text(airportPair.getValue()));
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
     }
 }
