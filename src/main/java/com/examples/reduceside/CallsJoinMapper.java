175648f9496649c9c8f98474c5290baf12e9f86e
nika <pinskaya.nika@mail.ru>
2019-10-18 17:19:19 +0300

    Scripted auto-commit on change (2019-10-18 17:19:19) by gitwatch.sh

--- src/main/java/com/examples/reduceside/CallsJoinMapper.java
+++ src/main/java/com/examples/reduceside/CallsJoinMapper.java
@@ -1,4 +1,9 @@
 package com.examples.reduceside;
 
-public class CallsJoinMapper {
+import javax.naming.Context;
+
+public class CallsJoinMapper<LongWritable, Text, TextPair, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context,)
+
 }
