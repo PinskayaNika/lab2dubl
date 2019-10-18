5eab64d1b09b3c8b0efd976bc8f90feb459ebb3c
nika <pinskaya.nika@mail.ru>
2019-10-18 15:52:02 +0300

    Scripted auto-commit on change (2019-10-18 15:52:02) by gitwatch.sh

--- src/main/java/com/examples/reduceside/Slide.java
+++ src/main/java/com/examples/reduceside/Slide.java
@@ -3,6 +3,10 @@ package com.examples.reduceside;
 public class Slide {
 
     public static void main(String[] args) throws Exception {
-
+        if (args.length != 2) {
+            System.err.println("Usage: Slide <input path> <output path>");
+            System.exit(-1);
+        }
+        
     }
 }
