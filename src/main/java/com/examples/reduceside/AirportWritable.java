4f57dba568d742ff8de01d8d43f38ffbf7b19d84
nika <pinskaya.nika@mail.ru>
2019-10-19 10:31:52 +0300

    Scripted auto-commit on change (2019-10-19 10:31:52) by gitwatch.sh

--- src/main/java/com/examples/reduceside/AirportWritable.java
+++ src/main/java/com/examples/reduceside/AirportWritable.java
@@ -1,4 +1,11 @@
 package com.examples.reduceside;
 
+import javafx.util.Pair;
+
 public class AirportWritable {
+    private Pair<String, String> airportPair;
+
+    public AirportWritable(String airport) {
+        
+    }
 }
