003e91efb92ecea0cb7905e7f24dfe06504c100d
nika <pinskaya.nika@mail.ru>
2019-10-21 16:07:15 +0300

    Scripted auto-commit on change (2019-10-21 16:07:15) by gitwatch.sh

--- src/main/java/com/examples/reduceside/AirportMapper.java
+++ src/main/java/com/examples/reduceside/AirportMapper.java
@@ -8,10 +8,20 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, JoinPair, Text> {
+    private static final String EMPTY = "";
+    private static final String QUITE = "\"";
+
+
+    public static String remote(String string) {
+        return  string.replaceAll(EMPTY, QUITE);
+    }
+
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         AirportWritable airportWritable = new AirportWritable(value.toString());
         Pair<String, String> airportPair = airportWritable.getAirportPair();
+
         try {
             context.write(new JoinPair(airportPair.getKey(), 0), new Text(airportPair.getValue()));
         } catch (NullPointerException e) {
