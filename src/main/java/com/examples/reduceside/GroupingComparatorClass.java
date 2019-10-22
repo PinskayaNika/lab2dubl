925058afe535129bcdfc8d936ede264031463d0a
nika <pinskaya.nika@mail.ru>
2019-10-22 12:08:24 +0300

    Scripted auto-commit on change (2019-10-22 12:08:24) by gitwatch.sh

--- src/main/java/com/examples/reduceside/GroupingComparatorClass.java
+++ src/main/java/com/examples/reduceside/GroupingComparatorClass.java
@@ -4,18 +4,29 @@ import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 //Учитывает только код аэропорта
-public class GroupingComparatorClass extends WritableComparator {
+//public class GroupingComparatorClass extends WritableComparator {
+//
+//    protected GroupingComparatorClass() {     //Проверка кода аэропорта
+//        super(JoinPair.class, true);
+//    }
+//
+//    @Override
+//    public int compare(WritableComparable a, WritableComparable b) {
+//        JoinPair Key1 = (JoinPair) a;
+//        JoinPair Key2 = (JoinPair) b;
+//
+//        return Key1.compareToFirstPart(Key2);
+//    }
+//}
 
-    protected GroupingComparatorClass() {     //Проверка кода аэропорта
+public class GroupingComparatorClass extends WritableComparator {
+    protected GroupingComparatorClass(){
         super(JoinPair.class, true);
     }
 
-    @Override
-    public int compare(WritableComparable a, WritableComparable b) {
-        JoinPair Key1 = (JoinPair) a;
-        JoinPair Key2 = (JoinPair) b;
-
-        return Key1.compareToFirstPart(Key2);
+    public int compare(WritableComparable first, WritableComparable second){
+        JoinPair f =(JoinPair) first;
+        JoinPair s =(JoinPair) second;
+        return f.compareToFirstPart(s);
     }
 }
-
