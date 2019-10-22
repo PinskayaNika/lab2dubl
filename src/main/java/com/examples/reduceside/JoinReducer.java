6424584cb8162f5a625525326abd93cdeff5e970
nika <pinskaya.nika@mail.ru>
2019-10-22 14:20:58 +0300

    Scripted auto-commit on change (2019-10-22 14:20:58) by gitwatch.sh

--- src/main/java/com/examples/reduceside/JoinReducer.java
+++ src/main/java/com/examples/reduceside/JoinReducer.java
@@ -1,14 +1,74 @@
 package com.examples.reduceside;
+//
+//import org.apache.hadoop.mapreduce.Reducer;
+//
+//import org.apache.hadoop.io.Text;
+//import java.io.IOException;
+//import java.util.Iterator;
+//
+//
+//public class JoinReducer extends Reducer<JoinPair, Text, Text, Text>  {
+//    @Override
+//    protected void reduce(JoinPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+//        /*int counter = 0;
+//        Double min = Double.MAX_VALUE;
+//        Double max = Double.MIN_VALUE;
+//        Double currentDelay, sum = 0.0;
+//*/
+//        Iterator iterator = values.iterator();
+//       // String airportName = iterator.next().toString();
+//        Text airportName = new Text(iterator.next().toString());
+//
+//
+//        if (iterator.hasNext()) {
+//            int counter = 0;
+//            Double min = Double.MAX_VALUE;
+//            Double max = Double.MIN_VALUE;
+//            Double currentDelay, sum = 0.0;
+//            while (iterator.hasNext()) {
+//                String call = iterator.next().toString();
+//
+//                if (call.length() == 0) {
+//                    continue;
+//                }
+//
+//                currentDelay = new Double(call);
+//
+//                if (currentDelay == 0.0) {
+//                    continue;
+//                }
+//
+//                if (counter == 0 || max < currentDelay) {
+//                    max = currentDelay;
+//                }
+//
+//                if (counter == 0 || min > currentDelay) {
+//                    min = currentDelay;
+//                }
+//
+//                sum = sum + currentDelay;
+//                counter = counter + 1;
+//            }
+//
+//                String outValue = "".concat("[Min: ".concat(min.toString().concat(", ")));
+//                outValue = outValue.concat("Max: ").concat(max.toString()).concat(", ");
+//                outValue = outValue.concat("Avg: ").concat(new Double(Math.round(10.0 * (sum / counter)) / 10.0).toString().concat("];"));
+//                context.write(new Text(airportName), new Text(outValue));
+//        }
+//
+//    }
+//}
 
-import org.apache.hadoop.mapreduce.Reducer;
+
 
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
 import java.io.IOException;
 import java.util.Iterator;
 
 
-public class JoinReducer extends Reducer<JoinPair, Text, Text, Text>  {
-    @Override
+public class JoinReducer extends Reducer<JoinPair, Text, Text, Text> {
     protected void reduce(JoinPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         /*int counter = 0;
         Double min = Double.MAX_VALUE;
@@ -16,9 +76,7 @@ public class JoinReducer extends Reducer<JoinPair, Text, Text, Text>  {
         Double currentDelay, sum = 0.0;
 */
         Iterator iterator = values.iterator();
-       // String airportName = iterator.next().toString();
         Text airportName = new Text(iterator.next().toString());
-
 
         if (iterator.hasNext()) {
             int counter = 0;
@@ -49,11 +107,21 @@ public class JoinReducer extends Reducer<JoinPair, Text, Text, Text>  {
                 sum = sum + currentDelay;
                 counter = counter + 1;
             }
-
-                String outValue = "".concat("[Min: ".concat(min.toString().concat(", ")));
-                outValue = outValue.concat("Max: ").concat(max.toString()).concat(", ");
-                outValue = outValue.concat("Avg: ").concat(new Double(Math.round(10.0 * (sum / counter)) / 10.0).toString().concat("];"));
-                context.write(new Text(airportName), new Text(outValue));
+              /*  current = Float.parseFloat(iterator.next().toString());
+                if (current < min || counter==0)
+                    min = current;
+                else if (current > max)
+                    max = current;
+                sum += current;
+                counter++;
+            }
+            sum /= counter;*/
+            String outValue = "".concat("[Min: ".concat(min.toString().concat(", ")));
+            outValue = outValue.concat("Max: ").concat(max.toString()).concat(", ");
+            outValue = outValue.concat("Avg: ").concat(new Double(Math.round(10.0 * (sum / counter)) / 10.0).toString().concat("];"));
+            context.write(new Text(airportName), new Text(outValue));
+            //  Text Result = new Text("min: " + Float.toString(min) + ", max: " + Float.toString(max) + ", diff: " + Float.toString(sum));
+            // context.write(airportName, Result);
         }
 
     }
