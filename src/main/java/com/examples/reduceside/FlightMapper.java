09edb33a3a1301eca0b0655b16c5f672a73f64cf
nika <pinskaya.nika@mail.ru>
2019-10-22 13:56:50 +0300

    Scripted auto-commit on change (2019-10-22 13:56:50) by gitwatch.sh

--- src/main/java/com/examples/reduceside/FlightMapper.java
+++ src/main/java/com/examples/reduceside/FlightMapper.java
@@ -1,6 +1,45 @@
 package com.examples.reduceside;
+//
+//import javafx.util.Pair;
+//import org.apache.hadoop.io.LongWritable;
+//import org.apache.hadoop.io.Text;
+//import org.apache.hadoop.mapreduce.Mapper;
+//
+//import java.io.IOException;
+//import java.io.OptionalDataException;
+//import java.util.Optional;
+//
+//public class FlightMapper extends Mapper<LongWritable, Text, JoinPair, Text> {
+//    private final static int AIRPORT_DESTINATION_ID = 14;
+//    private final static int TOTAL = 18;
+//    private static final String DELIMITER = ",";
+//
+//
+//    private Optional<String> getOptionalDelay(String string) {
+//        if (string.isEmpty() || Float.parseFloat(string) < 0.0f) {
+//            return Optional.empty();
+//        } else {
+//            return Optional.of(string);
+//        }
+//    }
+//
+//    @Override
+//    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+//        int airportId;
+//        String[] string = value.toString().split(DELIMITER);
+//        if (key.get() > 0) {
+//            airportId = Integer.parseInt(string[AIRPORT_DESTINATION_ID]);
+//            Optional<String> delay = getOptionalDelay(string[TOTAL]);
+//
+//            if (delay.isPresent()) {
+//                JoinPair Key = new JoinPair(airportId, 1);
+//                context.write(Key, new Text(delay.get()));
+//            }
+//
+//        }
+//    }
+//}
 
-import javafx.util.Pair;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
--- src/main/java/com/examples/reduceside/Slide.java
+++ src/main/java/com/examples/reduceside/Slide.java
@@ -47,50 +47,6 @@ package com.examples.reduceside;
 //}
 //
 
-//import org.apache.hadoop.fs.Path;
-//import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-//import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-//import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
-//import org.apache.hadoop.mapreduce.Job;
-//
-//import org.apache.hadoop.io.Text;
-//
-//public class SlideJoin {
-//    private final static String FLIGHT_CSV_PATH = "664600583_T_ONTIME_sample.csv";
-//    private final static String AIRPORT_CSV_PATH = "L_AIRPORT_ID.csv";
-//    //public final static String DELIMITER = ",";
-//
-//    public static void main(String[] args) throws Exception {
-//        if (args.length != 3) {
-//            System.err.println("Usage: Slide <output path>");
-//            System.exit(-1);
-//        }
-//        Job job = Job.getInstance();
-//        job.setJarByClass(SlideJoin.class);
-//        job.setJobName("JoinJob.sort");
-//        //MultipleInputs.addInputPath(job, new Path(FLIGHT_CSV_PATH), TextInputFormat.class, FlightMapper.class);
-//        //MultipleInputs.addInputPath(job, new Path(AIRPORT_CSV_PATH), TextInputFormat.class, AirportMapper.class);
-//
-//
-//        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
-//        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
-//
-//
-//
-//        FileOutputFormat.setOutputPath(job, new Path(args[2]));
-//
-//        job.setReducerClass(JoinReducer.class);
-//        job.setPartitionerClass(FlightPartitioner.class);
-//        job.setGroupingComparatorClass(GroupingComparatorClass.class);
-//
-//        job.setMapOutputKeyClass(JoinPair.class); //Для двойного ключа
-//        job.setMapOutputValueClass(Text.class);
-//        job.setOutputKeyClass(Text.class);
-//        job.setOutputValueClass(Text.class);
-//        job.setNumReduceTasks(2);
-//        System.exit(job.waitForCompletion(true) ? 0 : 1);
-//    }
-//}
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
