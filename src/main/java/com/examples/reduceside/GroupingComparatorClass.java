1aed6c24ed703fdc5a90c7e6ac5ca9097ff57f3f
nika <pinskaya.nika@mail.ru>
2019-10-22 14:25:46 +0300

    Scripted auto-commit on change (2019-10-22 14:25:46) by gitwatch.sh

--- src/main/java/com/examples/reduceside/GroupingComparatorClass.java
+++ src/main/java/com/examples/reduceside/GroupingComparatorClass.java
@@ -1,24 +1,52 @@
 package com.examples.reduceside;
+//
+//import org.apache.hadoop.io.WritableComparable;
+//import org.apache.hadoop.io.WritableComparator;
+//
+////Учитывает только код аэропорта
+////public class GroupingComparatorClass extends WritableComparator {
+////
+////    protected GroupingComparatorClass() {     //Проверка кода аэропорта
+////        super(JoinPair.class, true);
+////    }
+////
+////    @Override
+////    public int compare(WritableComparable a, WritableComparable b) {
+////        JoinPair Key1 = (JoinPair) a;
+////        JoinPair Key2 = (JoinPair) b;
+////
+////        return Key1.compareToFirstPart(Key2);
+////    }
+////}
+//
+//public class GroupingComparatorClass extends WritableComparator {
+//    protected GroupingComparatorClass(){
+//        super(JoinPair.class, true);
+//    }
+//
+//    public int compare(WritableComparable first, WritableComparable second){
+//        JoinPair f =(JoinPair) first;
+//        JoinPair s =(JoinPair) second;
+//        return f.compareToFirstPart(s);
+//    }
+//}
 
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 //Учитывает только код аэропорта
-//public class GroupingComparatorClass extends WritableComparator {
-//
-//    protected GroupingComparatorClass() {     //Проверка кода аэропорта
-//        super(JoinPair.class, true);
-//    }
-//
-//    @Override
-//    public int compare(WritableComparable a, WritableComparable b) {
-//        JoinPair Key1 = (JoinPair) a;
-//        JoinPair Key2 = (JoinPair) b;
-//
-//        return Key1.compareToFirstPart(Key2);
-//    }
-//}
+/*public class GroupingComparatorClass extends WritableComparator {
 
+    protected GroupingComparatorClass() {     //Проверка кода аэропорта
+        super(JoinPair.class, true);
+    }
+
+    public int compare(WritableComparable a, WritableComparable b) {
+        JoinPair Key1 = (JoinPair) a;
+        JoinPair Key2 = (JoinPair) b;
+        return Key1.compareToFirstPart(Key2);
+    }
+}*/
 public class GroupingComparatorClass extends WritableComparator {
     protected GroupingComparatorClass(){
         super(JoinPair.class, true);
