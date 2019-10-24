87ab435c4987b2642527c6a835ccde1351f65c39
nika <pinskaya.nika@mail.ru>
2019-10-24 19:41:03 +0300

    Scripted auto-commit on change (2019-10-24 19:41:03) by gitwatch.sh

--- src/main/java/com/examples/reduceside/FlightPartitioner.java
+++ src/main/java/com/examples/reduceside/FlightPartitioner.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
 
 //Учитывает только код аэропорта
 public class FlightPartitioner extends Partitioner<JoinPair, Text> {
-    @Override
+  
     public int getPartition (JoinPair key, Text value, int numPartitions) {
      //   int airportID = new Integer(key.getAirportID());
      //   return  airportID % numPartitions;
